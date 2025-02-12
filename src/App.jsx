aba26a5925c57eff643027d83df40e28cffc6ac3
jejediniz <jessicadiniz1224@gmail.com>
2025-02-12 15:01:19 -0300

    InicioCalculator

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Calculator from "./components/Calculator.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", color: "#fff" }}>Calculadora</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
